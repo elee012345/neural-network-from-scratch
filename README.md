1b18c417ad6f26da014a0b1ec85f06306cb9dc23
ethan <ethanlee012345@gmail.com>
2023-06-16 23:08:30 -0500

    initialization distributions
    commit before i go to sleep

--- README.md
+++ README.md
@@ -8,3 +8,10 @@ I am trying to write a basic neural network like how the book describes
 so it's probably not going to be written the best lol
 
 totally did not steal the mnist loader and files from the link above
+
+
+some good resources:
+https://course.fast.ai/index.html <- i love this man thank you so much for this
+http://neuralnetworksanddeeplearning.com/index.html
+https://ml-cheatsheet.readthedocs.io/en/latest/index.html
+https://forums.fast.ai/
--- network.py
+++ network.py
@@ -1,9 +1,17 @@
 import numpy as np
 
 class Network():
+    # apparently a gaussian/normal distribution works well lol
+    # linear does as well, but in the example he uses normal so ¯\_(ツ)_/¯
 
-    def __init__(self) -> None:
-        pass
+    # ok nevermind there is a method to this madness
+    # https://adityassrana.github.io/blog/theory/2020/08/26/Weight-Init.html
+    # https://www.youtube.com/watch?v=vGsc_NbU7xc&t=965s
+
+    # normal distributions aren't the best, they're okay, but for this simple network it's good enough for me
+
+    def __init__(self, layers):
+        self.biases = [np.random.randn(1, )]
 
 
 def sigmoid(z):
